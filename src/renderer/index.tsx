055e856af1c4ece7814447dadc8dc8e76f185f19
Sébastien Galoux <sga@pulsar.be>
2018-02-14 22:12:03 +0100

    Add tslint

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>React Applicdation</div>, document.getElementById('app'))
+ReactDOM.render(<div>React Applicdation</div>, document.getElementById('app'));
